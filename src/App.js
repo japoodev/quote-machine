88f8f3b87aac41865d287bd805a62683ba0ece89
japoodev <jafarsaadique@gmail.com>
2022-10-10 00:31:16 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+// Intialize newQuote and author state
+
+// Fetch a new quote with author from the API and store it in state
+
+// create a list of black shades
+const shades = [
+  '#16a085',
+  '#27ae60',
+  '#2c3e50',
+  '#f39c12',
+  '#e74c3c',
+  '#9b59b6',
+  '#FB6964',
+  '#342224',
+  '#472E32',
+  '#BDBB99',
+  '#77B1A9',
+  '#73A857'
+]
+
+const NewQuote = ({ newQuote, color }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <button
+      style={{ backgroundColor: shades[color] }}
+      id="new-quote"
+      onClick={newQuote}>New Quote
+    </button>
+  )
 }
 
-export default App;
+const TweetQuote = ({ quote, author, color }) => {
+  return (
+    <a
+      style={{ backgroundColor: shades[color] }}
+      id="tweet-quote"
+      href={`https://twitter.com/intent/tweet?text=${quote} - ${author}`}
+      target="_blank"
+      rel="noopener noreferrer">Tweet {' '}
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-twitter" viewBox="0 0 16 16">
+  <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"/>
+</svg>
+    </a>
+  )
+}
+
+const App = () => {
+  const [quote, setQuote] = React.useState('')
+  const [author, setAuthor] = React.useState('')
+  const [color, setColor] = React.useState(Math.floor(Math.random() * shades.length))
+  const newQuote = () => {
+    fetch('https://api.quotable.io/random')
+      .then(response => response.json())
+      .then(data => {
+        setQuote(data.content)
+        setAuthor(data.author)
+      })
+      setTimeout(setColor(Math.floor(Math.random() * shades.length)), 4000)
+  }
+  React.useEffect(newQuote, [])
+  return (
+
+    <div class="container" style={{backgroundColor: shades[color]}}>
+      <div id="quote-box">
+        <Quotes
+          quote={quote}
+          author={author}
+          color={color} />
+        <div className="buttons">
+          <TweetQuote quote={quote} author={author} color={color} />
+          <NewQuote newQuote={newQuote} color={color}/>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const Quotes = ({ quote, author, color }) => {
+  return (
+    <div style={{color: shades[color]}}>
+      <h1 id="text">"{quote}"</h1>
+      <p id="author">- {author}</p>
+    </div>
+  )
+}
+export default App
